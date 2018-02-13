42868a168fb00895577d23af74e0d7d9b3225447
staxDB <borndavid508@gmail.com>
2018-02-13 09:35:18 +0100

    changed notification translation

--- messages/de/widgets_views_myTasks.php
+++ messages/de/widgets_views_myTasks.php
@@ -1,6 +1,6 @@
 <?php
 return array (
-    '<strong>Your</strong> tasks' => '<strong>Ihre</strong> Aufgaben',
+    '<strong>Your</strong> tasks' => '<strong>Deine</strong> Aufgaben',
     'Responsible' => 'Verantwortlich',
     'Assigned' => 'Zugewiesen',
     'For all' => 'Für alle'
