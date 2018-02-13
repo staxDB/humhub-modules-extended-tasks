4549f72c35339cdb07e686383b32e8fcf97790a7
staxDB <borndavid508@gmail.com>
2018-02-13 09:34:25 +0100

    changed notification translation

--- messages/de/notifications.php
+++ messages/de/notifications.php
@@ -1,17 +1,16 @@
 <?php
 return array (
     '{userName} requests a deadline extension for task {task} in space {spaceName}.' => '{userName} fordert eine Fristverlängerung für die Aufgabe {task} im Space {spaceName}.',
-    '{userName} has rejected your deadline extension request for task {task} in space {spaceName}.' => '{userName} hat Ihren Antrag auf Fristverlängerung für die Aufgabe {task} im Space {spaceName} abgelehnt.',
-    '{userName} assigned you to task {task} in space {spaceName}.' => '{userName} hat Sie der Aufgabe {task} im Space {spaceName} zugewiesen.',
-    '{userName} assigned you as responsible person in task {task} in space {spaceName}.' => '{userName} hat Sie als verantwortliche Person in Aufgabe {task} im Space {spaceName} zugewiesen.',
+    '{userName} assigned you to task {task} in space {spaceName}.' => '{userName} hat dich der Aufgabe {task} im Space {spaceName} zugewiesen.',
+    '{userName} has changed deadline for task {task} in space {spaceName}.' => '{userName} hat die Deadline für die Aufgabe {task} im Raum {spaceName} geändert.',
+    '{userName} assigned you as responsible person in task {task} from space {spaceName}.' => '{userName} hat dich als verantwortliche Person in Aufgabe {task} im Space {spaceName} zugewiesen.',
     '{userName} marked Task {task} in space {spaceName} as completed.' => '{userName} markierte Aufgabe {task} im Space {spaceName} als abgeschlossen.',
     '{userName} completed Task {task} in space {spaceName}.' => '{userName} hat Aufgabe {task} im Space {spaceName} abgeschlossen.',
     '{userName} started working on Task {task} in space {spaceName}.' => '{userName} hat begonnen an Aufgabe {task} im Space {spaceName} zu arbeiten.',
-    '{userName} requests you to review Task {task} in space {spaceName}.' => '{userName} hat Sie angefragt, Aufgabe {task} im Space {spaceName} zu überprüfen.',
+    '{userName} requests you to review Task {task} in space {spaceName}.' => '{userName} hat dich angefragt, Aufgabe {task} im Space {spaceName} zu überprüfen.',
     '{userName} has rejected task {task} in space {spaceName} as incomplete.' => '{userName} hat Aufgabe {task} im Space {spaceName} als unvollständig deklariert.',
     '{userName} has re-opened task {task} in space {spaceName}.' => '{userName} hat Aufgabe {task} im Space {spaceName} wieder geöffnet.',
     'Task {task} in space {spaceName} ends at {dateTime}.' => 'Aufgabe {task} im Space {spaceName} endet am {dateTime}.',
     'Task {task} in space {spaceName} starts at {dateTime}.' => 'Aufgabe {task} im Space {spaceName} beginnt am {dateTime}.',
-    '{userName} has changed deadline for {task} in space {spaceName}.' => '{userName} hat die Deadline der Aufgabe {task} im Space {spaceName} geändert.',
     'Reminder' => 'Erinnerung',
 );
