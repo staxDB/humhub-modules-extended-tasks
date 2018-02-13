b39f4dcafe617f8a7396ca76a4925d4038caaa13
staxDB <borndavid508@gmail.com>
2018-02-13 09:09:08 +0100

    updated notifications/base.php

--- messages/de/base.php
+++ messages/de/base.php
@@ -1,7 +1,5 @@
 <?php
 return array(
-    '<strong>Duplicate</strong> task' => '<strong>Aufgabe</strong> kopieren',
-    '<strong>Shift</strong> agenda entry to new task' => '<strong>Verschiebe</strong> Checklisten-Punkt in neue Aufgabe',
     'Adds a task manager to this space.' => 'Fügt diesem Space einen Task-Manager hinzu.',
     'Back to overview' => 'Zurück zur Übersicht',
     'Delete' => 'Löschen',
@@ -9,8 +7,6 @@ return array(
     'Close' => 'Schließen',
     'Task' => 'Aufgabe',
     'Tasks' => 'Aufgaben',
-    'Move down' => 'Nach unten verschieben',
-    'Move up' => 'Nach oben verschieben',
     'Shift to other task' => 'Anderer Aufgabe zuordnen',
     '“Tasks” is a complete task manager, which enables you to easily create tasks, define checklists and assign users and responsible users.' => '“Tasks” ist ein kompletter Taskmanager, der es Ihnen ermöglicht, auf einfache Weise Aufgaben zu erstellen, Checklisten zu definieren und Benutzer sowie verantwortliche Personen zuzuordnen.'
 );
