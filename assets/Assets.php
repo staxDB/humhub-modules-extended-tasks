a517377b7fbef0def40ce6fdb0aee8734ece27b8
staxDB <borndavid508@gmail.com>
2018-02-22 17:36:34 +0100

    Added homepage-link for assets library

--- assets/Assets.php
+++ assets/Assets.php
@@ -26,7 +26,7 @@ class Assets extends AssetBundle
 
     // We have to use the timeentry lib for the duration since the TimePicker widget uses an older version without maxHour setting...
     public $js = [
-        'js/jquery.ui.touch-punch.min.js',
+        'js/jquery.ui.touch-punch.min.js',  // Add jQuery fix for using sortable() on mobile devices - Homepage: http://touchpunch.furf.com/
         'js/humhub.task.js',
     ];
 }
