cfd630a190ca701a4e100762bad3c2c0511cb9ee
staxDB <borndavid508@gmail.com>
2018-02-27 17:21:51 +0100

    fixed translation

--- messages/de/models_forms_TaskFilter.php
+++ messages/de/models_forms_TaskFilter.php
@@ -3,6 +3,6 @@ return array (
     'Filter tasks' => 'Aufgaben filtern',
     'Overdue' => 'Überfällig',
     'I\'m assigned' => 'Ich bin zugeordnet',
-    'I\'m responsible' => 'Ich bin verantworlich',
+    'I\'m responsible' => 'Ich bin verantwortlich',
     'Created by me' => 'Von mir erstellt'
 );
